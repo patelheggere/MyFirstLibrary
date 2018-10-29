c9c34113ca5be719d841d2eecd9da92a52be8e00
Veerendra patel <patelheggere@gmail.com>
2018-10-29 15:55:22 +0530

    test

---
+++ app/src/main/java/com/patelheggere/myfirstlibrary/TetsClass.java
@@ -0,0 +1,19 @@
+package com.patelheggere.myfirstlibrary;
+
+public class TetsClass {
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    String test;
+    TetsClass()
+    {
+        this.test = "Veerendra";
+    }
+
+
+}
